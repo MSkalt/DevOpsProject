cc70ecbfa6eaa33bfc15030c84246eda4783c866
Max Skalt <mskalt@gmail.com>
2024-05-03 14:58:47 +0300

    Add comment to file

---
+++ start_apps.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def start_app(path):
+    return subprocess.Popen(['python', path])
+
+if __name__ == "__main__":
+    print("Starting REST API...")
+    rest_api = start_app('rest_app.py')
+
+    print("Starting Web Interface...")
+    web_app = start_app('web_app.py')
+
+    rest_api.wait()
+    web_app.wait()
+#comment 
