c6b452ce2215753218e3e65f9d09fb33754cd1b5
Max Skalt <mskalt@gmail.com>
2024-05-05 14:33:00 +0300

    Run start_apps.py minimized in the background

--- start_apps.py
+++ start_apps.py
@@ -1,14 +1,13 @@
 import subprocess
 
-def start_app(path):
-    return subprocess.Popen(['python', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def start_app(command):
+    return subprocess.Popen(command, shell=True)
 
 if __name__ == "__main__":
     print("Starting REST API...")
-    rest_api = start_app('rest_app.py')
+    rest_api = start_app('python rest_app.py')
 
     print("Starting Web Interface...")
-    web_app = start_app('web_app.py')
+    web_app = start_app('python web_app.py')
 
-    # Do not wait for these processes to finish, they run in the background
     print("Applications are running in the background.")
